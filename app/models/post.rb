2a9d0f6e87e1b5cc9fcc76e4128763612442d61b
akimegane0730 <akimegane0730o@gmail.com>
2020-01-28 10:24:51 +0900

    create posts-controller create-action

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,6 @@ class Post < ApplicationRecord
   belong_to :user
 
   has_many :photos, dependent: :destroy
+
+  accepts_nested_attributes_for :photos
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root "pictures#index"
   resources :users, only: [:show]
-  resources :posts, only: [:new]
+  resources :posts, only: [:new, :create]
 end
